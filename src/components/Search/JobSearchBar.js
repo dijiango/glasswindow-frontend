03e2ece359fb97e80a68901d5209e70e9e8d7fe2
Diana Jiang <jiang99diana@gmail.com>
2022-03-04 16:47:10 -0500

    created job search bar/css file

---
+++ src/components/Search/JobSearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './JobSearch.css';
+
+
+function JobSearchBar() {
+  return (
+    <div className='job-searchbar-container'>
+        Search bar to filter jobs by company/position/etc
+    </div>
+  )
+}
+
+export default JobSearchBar
