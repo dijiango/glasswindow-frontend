daae76a7478e7d372ac103ec46b9b1041bde9bb2
Diana Jiang <jiang99diana@gmail.com>
2022-03-14 13:38:43 -0400

    added search bar and error handling

--- src/components/Search/JobSearch.js
+++ src/components/Search/JobSearch.js
@@ -8,6 +8,8 @@ import JobSearchBar from './JobSearchBar';
 function JobSearch(props) {
 
     const [jobData, setJobData] = useState([]);
+    const [searchError, setSearchError] = useState("")
+    const [refresh, setRefresh] = useState("");
 
     const fetchFunction = () => {
         fetch('http://localhost:9292/jobs')
@@ -15,9 +17,41 @@ function JobSearch(props) {
         .then(data => setJobData(data))}
     useEffect(fetchFunction, [])
 
+    // console.log(jobData[1].title);
+
+    function handleFilter(jobs){
+        // console.log("Only show these jobs", jobs.job);
+        const jobString = jobs.job.toLowerCase();
+        const locationString = jobs.location.toLowerCase();
+        
+        const result = jobData.filter((obj)=> {
+            if (jobs.job !== '' && jobs.location === '') {
+                return obj.title.toLowerCase().includes(jobString) || obj.company.name.toLowerCase().includes(jobString)
+            } else if (jobs.job !== '' && jobs.location !== '') {
+                return (obj.title.toLowerCase().includes(jobString) && obj.location.toLowerCase().includes(locationString)) || obj.company.name.toLowerCase().includes(jobString) && obj.location.toLowerCase().includes(locationString)    
+            } else if (jobs.job === '' && jobs.location !== '') {
+                return obj.location.toLowerCase().includes(locationString)
+            } else if (jobs.job === '' && jobs.location === '') {
+                return obj.title.length >= 0;
+            }
+            
+        })
+        
+        console.log(result);
+        if (result.length === 0) {
+            setSearchError("No matches");
+            setRefresh("Show all jobs")
+        } else {
+            setSearchError("")
+            setRefresh("")
+        }
+
+        setJobData(result);
+    }
+
     return (
         <div>
-            <JobSearchBar />
+            <JobSearchBar job={jobData} setJobs={setJobData} handleFilter={handleFilter} searchError={searchError} refresh={refresh}/>
         <div className='search-page'>
             {jobData.map(
                 (eachJob) => {
--- src/components/Search/JobSearchBar.js
+++ src/components/Search/JobSearchBar.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Button, FormGroup, Paper, TextField } from '@mui/material';
 import './Job.css';
 
 
-function JobSearchBar() {
+const defaultValues = {
+  job: '',
+  location: '',
+};
+
+function JobSearchBar( props ) {
+  const [values, setValues] = useState(defaultValues);
+
+  function handleInput(e) {
+    
+    const value = e.target.value;
+    setValues({
+      ...values,
+      [e.target.name]: value,
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.handleFilter(values);
+    setValues(defaultValues);
+  }
   return (
-    <div className='job-searchbar-container'>
-        Search bar to filter jobs by company/position/etc
+    <div className='job-searchbar-wrapper'>
+      <Paper className='job-searchbar-container' elevation={0}>
+        <form onSubmit={handleSubmit}>
+          <FormGroup row={true} sx={{margin: '0px 25px 0px', alignItems:'center', justifyContent:'center'}}>
+        <label className='searchbar-label'>Search By...</label>
+            <TextField 
+              sx={{margin: '10px'}}
+              placeholder="Search by title or company"
+              name='job'
+              value={values.job}
+              onChange={handleInput}
+            />
+            <TextField
+              sx={{margin: '10px'}}
+              placeholder="Search by state"
+              name='location'
+              value={values.location}
+              onChange={handleInput}
+              />  
+            <Button type='submit' color='success' variant='contained'>Submit</Button>
+          </FormGroup>
+          <span className='error'>{props.searchError}</span>
+          <span className='refresh' onClick={()=> window.location.reload()}>{props.refresh}</span>
+          
+        </form>
+      </Paper>
     </div>
   )
 }
